a2af40cffac234a7a5d3dd7445c1bd34dd67a380
Jeonghyun-S <jingjingtapa@rideflux.com>
2024-08-20 18:57:38 +0900

    remove gray input(2)

--- RoadStatusModel/practice/test5.py
+++ RoadStatusModel/practice/test5.py
@@ -2,7 +2,7 @@ import csv, os
 
 def csvwriter(csv_dir, target_list):
     with open(csv_dir, 'w', newline="") as file:
-        writer = csv.writer(file,quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(file)
         writer.writerows(target_list)
     print(f'List Saved at {csv_dir} Successfully')
 
@@ -10,19 +10,10 @@ annotation_file = 'test6.csv'
 with open(annotation_file,'r',newline='') as f:
     data = list(csv.reader(f))
 
-
-dir = '/media/ampere_2_1/T7/2024-Summer-Internship/NIA2021/'
-nia = []
-for scene in data:
-    scene_dir = dir+scene[0]+'/image0'
-    img_list = os.listdir(scene_dir)
-    cnt = 0
-    for img in img_list:
-        if cnt % 10 ==4:
-            x = f'/NIA2021/{scene[0]}/image0/{img}'
-            nia.append([x])
-        cnt += 1
-
-print(len(nia))
-    
-csvwriter('nia.csv',nia)
+result = []
+cnt = 0
+for img in data:
+    if cnt % 10 == 0:
+        result.append([img[0],img[1]])
+    cnt += 1
+csvwriter('nia.csv',result)
